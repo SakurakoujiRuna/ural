ca306116b9463d7a67b8240b2a631c85bb67d686
Meiku <ca72god@gmail.com>
2014-05-09 04:14:30 +0800

    dp

---
+++ 1009.rb
@@ -0,0 +1,11 @@
+n = gets.to_i
+k = gets.to_i
+f = Array.new(19) { Array.new(2) { 0 } }
+f[0][1] = 1
+
+0.upto(n-1) do |i|
+	f[i+1][0] = f[i][1]
+	f[i+1][1] = (k-1)*(f[i][0]+f[i][1])
+end
+
+print f[n][1],?\n
